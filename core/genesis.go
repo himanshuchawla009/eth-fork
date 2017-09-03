87867b7851d02d0260525fa547dec6750151844b
janruls1 <janruls1@darwinlabs.io>
2017-09-03 18:36:32 +0530

    Increased GasLimit of default genesis block

--- core/genesis.go
+++ core/genesis.go
@@ -312,9 +312,8 @@ func DefaultGenesisBlock() *Genesis {
 		Config:     params.MainnetChainConfig,
 		Nonce:      66,
 		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
-		GasLimit:   5000,
+		GasLimit:   5000000000,
 		Difficulty: big.NewInt(17179),
-		//Alloc:      decodePrealloc(mainnetAllocData),
 	}
 }
 
